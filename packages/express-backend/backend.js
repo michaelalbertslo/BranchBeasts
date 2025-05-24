ebab67dcaa32ea04087f910dd2a7291f05d63909
christophertu8 <ctu07@calpoly.edu>
2025-05-24 11:22:55 -0700

    Add auth.js and backend updates for authentication

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,6 +6,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
 import clothingItemService from "./services/clothingItem-service.js";
+import { registerUser, loginUser, authenticateUser } from "./auth.js";
 
 const {
   getClothingItems,
@@ -29,6 +30,8 @@ const app = express();
 const port = 8000;
 app.use(cors());
 app.use(express.json());
+app.post("/signup", registerUser);
+app.post("/login", loginUser);
 
 /** Get one item by its Mongo _id */
 app.get("/items/:id", (req, res) => {
@@ -46,7 +49,7 @@ app.get("/items/:id", (req, res) => {
 });
 
 /** List items (optionally filter by user_id) */
-app.get("/items", (req, res) => {  // /items shows clothing_items
+app.get("/items", authenticateUser, (req, res) => {  // /items shows clothing_items
   const filter = {};
   if (req.query.user_id) filter.user_id = req.query.user_id;
 
@@ -61,7 +64,7 @@ app.get("/items", (req, res) => {  // /items shows clothing_items
 });
 
 /** Create a new clothing item */
-app.post("/items", (req, res) => {
+app.post("/items", authenticateUser, (req, res) => {
   addClothingItem(req.body)
     .then((saved) => {
       res.status(201).json(saved);
@@ -73,7 +76,7 @@ app.post("/items", (req, res) => {
 });
 
 /** Delete an item by its Mongo _id */
-app.delete("/items/:id", (req, res) => {
+app.delete("/items/:id", authenticateUser, (req, res) => {
   deleteClothingItemById(req.params.id)
     .then((deleted) => {
       if (!deleted)
