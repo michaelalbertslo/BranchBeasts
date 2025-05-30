d96c6ec7c32e41b6e485b64e897b780bc1f38b5b
michaelalbertslo <mwalbert@calpoly.edu>
2025-05-30 10:50:04 -0700

    getting hosting to work - backend

--- packages/react-frontend/src/azure.jsx
+++ packages/react-frontend/src/azure.jsx
@@ -2,4 +2,4 @@ const isLocal = false;  // flip this once before deploying
 
 export const API_BASE_URL = isLocal
   ? 'http://localhost:8000'
-  : 'http://mycloset-hraqchf2h6a3gacc.westus2-01.azurewebsites.net';
+  : 'https://mycloset-hraqchf2h6a3gacc.westus2-01.azurewebsites.net';
