6e3e827d9a0bec7a7bbec8bb3be1310e0fe2eef2
michaelalbertslo <mwalbert@calpoly.edu>
2025-06-03 16:43:33 -0700

    changing backend urls

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -29,7 +29,7 @@ function MyApp() {
 
 
   function loginUser(creds) {
-    fetch("http://localhost:8000/login", {
+    fetch(`${API_BASE_URL}/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(creds)
@@ -59,7 +59,7 @@ function MyApp() {
 }
 
 function signupUser(creds) {
-  fetch("http://localhost:8000/signup", {
+  fetch(`${API_BASE_URL}/signup`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(creds),
