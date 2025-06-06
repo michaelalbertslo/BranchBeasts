6198ccde1e848b62dd512f1a205ff85dfe75741f
michaelalbertslo <mwalbert@calpoly.edu>
2025-06-06 12:25:16 -0700

    trying to resolve CI fails

--- packages/react-frontend/eslint.config.js
+++ packages/react-frontend/eslint.config.js
@@ -5,6 +5,40 @@ import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
   { ignores: ["dist"] },
+  //this should remove errors on the test files
+  {
+    files: ["**/*.{test,spec}.js"],
+
+    languageOptions: {
+      ecmaVersion: 2020,
+      // merge browser + jest globals
+      globals: { ...globals.browser, ...globals.jest },
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+
+    rules: {
+      // you can inherit the same base rules here if you want
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "no-unused-vars": [
+        "error",
+        { varsIgnorePattern: "^[A-Z_]" },
+      ],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
